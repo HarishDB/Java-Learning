ab5bf01058af7253e51b52aa22af377dfb9b5a0e
HarishDB <91124976+HarishDB@users.noreply.github.com>
2021-09-21 16:32:45 +0530

    Trial code

---
+++ FirstOne/src/com/harish/FirstOne.java
@@ -0,0 +1,10 @@
+package com.harish;
+
+public class FirstOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello       oooY");
+	}
+
+}
